4573321fed00787948051a41358ce4ca630bf5ab
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-02-17 14:33:29 +0100

    versiones estabilizadas

--- pagoTienda/src/main/java/gpl/granpepinillo/pagoTienda/PagoTienda.java
+++ pagoTienda/src/main/java/gpl/granpepinillo/pagoTienda/PagoTienda.java
@@ -6,28 +6,30 @@ import gpl.granpepinillo.pagoTienda.productos.*;
 
 public class PagoTienda {
     
-    /*
-    Montamos una tienda con una caja
-    y le asignamos un empleado
-    */
-    
+    public static void main(String[] args) {
+        /*
+        Montamos una tienda con una caja
+        y le asignamos un empleado
+        */
+        System.out.println("Poronga");
 
-    /*
-    El cliente quiere comprar 3 productos:
-    un gel de baño, una caja de piezas de lego
-    y una bolsa de plátanos
-    */
+        /*
+        El cliente quiere comprar 3 productos:
+        un gel de baño, una caja de piezas de lego
+        y una bolsa de plátanos
+        */
 
-    /*
-    El cliente decide no comprar el gel de baño
-    */
+        /*
+        El cliente decide no comprar el gel de baño
+        */
 
-    /*
-    Los plátanos tienen un descuento del 20%
-    */
+        /*
+        Los plátanos tienen un descuento del 20%
+        */
 
-    /*
-    El cliente quiere realizar la compra
-    */
+        /*
+        El cliente quiere realizar la compra
+        */
+    }
 
 }
